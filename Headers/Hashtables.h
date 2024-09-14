b2d58ef4625146d29a2140439a12e60f98d27aec
Kamve Gwijana <gwijgwij011@gmail.com>
2024-09-14 13:29:36 +0200

    clean up code

--- Headers/Hashtables.h
+++ Headers/Hashtables.h
@@ -8,29 +8,4 @@ public:
 
 	HashTables();
 	~HashTables();
-
-	struct City
-	{
-		std::string Name = "";
-		uint64_t totalPopulation = 0;
-		double Latitude = 0.0, Longitude = 0.0;
-	};
-
-	void SetCityInformation(City& someCity, std::string name, uint64_t population, double latitude, double longitude);
-	
-	City GetCityInformation();
-
-	//hashing:
-	namespace std {
-
-		template<>
-		struct hash<City> {
-			size_t operator()(const City& key) {
-				//lets has the city name:
-				return hash<std::string>()(key.Name);
-			}
-		};
-	}
-private:
-
 };
--- SourceFiles/Hashtables.cpp
+++ SourceFiles/Hashtables.cpp
@@ -1,20 +1,22 @@
-#include "Headers/Hashtables.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
 
-HashTables::HashTables() {
-	/*constructor */ 
-}
-HashTables::~HashTables() {
-	/*destructor*/ 
-}
-
-void HashTables::SetCityInformation(City& someCity, std::string name, 
-	uint64_t population, double latitude, double longitude)
+//COPY AND PASTE THIS CODE INTO THE MAIN SOURCE FILE.
+struct City
 {
-	City someCity = { name, population, latitude, longitude };
-}
+	std::string Name = "";
+	uint64_t totalPopulation = 0;
+	double Latitude = 0.0, Longitude = 0.0;
+};
 
-City HashTables::GetCityInformation()
-{
+int main()
+{	
+	City city1 = { "Cape Town", 2500, -33.45, 13.75 };//dummy values.
+	City city2 = { "Joburg", 1560, 45.59, 26.76 };
+	City city3 = { "Gqeberha", 456, 55.58, 16.76 };
+	City city4 = { "Komani", 2908, 78.59, 34.32 };
+
 	std::unordered_map<std::string, City> citiesMap; //keys have to be hashable.
 
 	citiesMap["Cape Town"] = City{ "Cape Town", 2500, -33.45, 13.75 };
@@ -22,5 +24,10 @@ City HashTables::GetCityInformation()
 	citiesMap["Gqeberha"] = City{ "Gqeberha", 456, 55.58, 16.76 };
 	citiesMap["Komani"] = City{ "Komani", 2908, 78.59, 34.32 };
 
-	return citiesMap;
+	//declare and init a reference to a city:
+	City& refCapeTown = citiesMap["Cape Town"];
+
+	std::cout << refCapeTown.Name << std::endl;
+
+	return 0;
 }
